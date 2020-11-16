0eba6d9902ee5f77879354257b84e79fc28c5a24
kiwitasion <bratzabrata239@gmail.com>
2020-11-16 11:52:24 +0300

    Change mapper

--- src/main/java/ru/kivit/mappers/HotelMapper.java
+++ src/main/java/ru/kivit/mappers/HotelMapper.java
@@ -13,22 +13,7 @@ public class HotelMapper implements RowMapper<Hotel> {
 
         hotel.setHotel_id(rs.getLong("hotel_id"));
         hotel.setName(rs.getString("name"));
-        Hotel.ServiceLevel sl = Hotel.ServiceLevel.Low;
-        switch (rs.getString("service_level")){
-            case "Low":
-                sl = Hotel.ServiceLevel.Low;
-                break;
-            case "Medium":
-                sl = Hotel.ServiceLevel.Medium;
-                break;
-            case "High":
-                sl = Hotel.ServiceLevel.High;
-                break;
-            case "Lux":
-                sl = Hotel.ServiceLevel.Lux;
-                break;
-        }
-        hotel.setService_level(sl);
+        hotel.setService_level(Hotel.ServiceLevel.valueOf(rs.getString("service_level")));
 
         return hotel;
     }
